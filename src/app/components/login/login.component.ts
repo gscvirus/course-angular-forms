d963ea55300dd9d19cdbf0f926214679aabe79a5
Gonzalo Solano <gonzalo.solano@live.com>
2024-02-23 08:37:27 -0400

    Example with simple databind

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -11,6 +11,9 @@ import { ToastrService } from 'ngx-toastr';
 export class LoginComponent implements OnInit {
     showValues: boolean = false;
     showExtraEmail: boolean = false;
+    email: string = "";
+    password: string = "";
+    extraEmail: string = "";
 
     constructor(private toastrService: ToastrService, private router: Router) {
     }
@@ -18,14 +21,40 @@ export class LoginComponent implements OnInit {
     ngOnInit(): void {
     }
 
-    onShowExtraEmail(event: Event) {
+    public setEmail(event: Event): void {
+        this.email = (<HTMLInputElement>event.target).value;
+        this.extraEmail = this.email;
+    }
+
+    public setPassword(event: Event): void {
+        this.password = (<HTMLInputElement>event.target).value;
+    }
+
+    public setExtraEmail(event: Event): void {
+        this.extraEmail = (<HTMLInputElement>event.target).value;
+    }
+
+    onShowExtraEmail(event: Event): void {
         this.showExtraEmail = (<HTMLInputElement>event.target).checked;
     }
 
-    onShowValues(event: Event) {
+    onShowValues(event: Event): void {
         this.showValues = (<HTMLInputElement>event.target).checked;
     }
 
-    onSignIn() {
+    onSignIn(): void {
+        if (this.email === "admin@harriscomputer.com" && this.password === "Control123#") {
+            this.toastrService.success(`User: ${this.email} welcome!`);
+            this.router.navigate(["/home"]);
+        } else {
+            this.toastrService.error("Invalid user or password");
+            this.loadDefaultData("", "", "");
+        }
+    }
+
+    public loadDefaultData(email: string = "admin@harriscomputer.com", password: string = "Control123#", extraEmail: string = "administrator@harriscomputer.com"): void {
+        this.email = email;
+        this.password = password;
+        this.extraEmail = extraEmail;
     }
 }
